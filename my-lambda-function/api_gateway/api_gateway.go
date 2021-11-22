5217d0ca52aa3aed6f769bb65ea8fe96235b1c6e
Tom Söderlund <tom@tomsoderlund.com>
2021-11-22 13:43:52 +0100

    Remove line

--- my-lambda-function/api_gateway/api_gateway.go
+++ my-lambda-function/api_gateway/api_gateway.go
@@ -8,7 +8,6 @@ import (
 func ApiResponse(status int, body interface{}) (*events.APIGatewayProxyResponse, error) {
 	resp := events.APIGatewayProxyResponse{Headers: map[string]string{"Content-Type": "application/json"}}
 	resp.StatusCode = status
-
 	stringBody, _ := json.Marshal(body)
 	resp.Body = string(stringBody)
 	return &resp, nil
