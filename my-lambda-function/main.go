5a8bddaf0c1e8b803ee38332835474a699f04863
Tom Söderlund <tom@tomsoderlund.com>
2021-11-22 13:20:54 +0100

    api_gateway sub-package

--- my-lambda-function/main.go
+++ my-lambda-function/main.go
@@ -3,6 +3,8 @@ package main
 import (
   "fmt"
   "github.com/aws/aws-lambda-go/lambda"
+  "github.com/aws/aws-lambda-go/events"
+  "example.com/my-lambda-function/api_gateway" // "example.com/my-lambda-function" is the module name in go.mod
 )
 
 type ExampleEvent struct {
@@ -13,8 +15,8 @@ type ExampleResponse struct {
   Message string `json:"message"`
 }
 
-func HandleLambdaEvent(event ExampleEvent) (ExampleResponse, error) {
-  return ExampleResponse{Message: fmt.Sprintf("Hello! Dear %s!", event.Name)}, nil
+func HandleLambdaEvent(event ExampleEvent) (*events.APIGatewayProxyResponse, error) {
+  return api_gateway.ApiResponse(200, ExampleResponse{Message: fmt.Sprintf("Hello! Dear %s!", event.Name)})
 }
 
 func main() {
