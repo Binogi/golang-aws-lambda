fad8a0584c1c3e4c855b221b02585b454d0a53fa
Tom Söderlund <tom@tomsoderlund.com>
2021-12-03 08:40:59 +0100

    Print request object

--- my-lambda-function/main.go
+++ my-lambda-function/main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"example.com/my-lambda-function/api_gateway" // "example.com/my-lambda-function" is the module name in go.mod
 	"fmt"
-  "encoding/json"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
 type BodyParameters struct {
-  Name string `json:"name"`
+	Name string `json:"name"`
 }
 
 type ExampleResponse struct {
@@ -17,9 +17,15 @@ type ExampleResponse struct {
 }
 
 func HandleLambdaEvent(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-  body := BodyParameters{}
-  json.Unmarshal([]byte(req.Body), &body)
-	return api_gateway.ApiResponse(200, ExampleResponse{Message: fmt.Sprintf("Hello! Dear %s!", body.Name)})
+	body := BodyParameters{}
+	json.Unmarshal([]byte(req.Body), &body)
+
+	// Print request object
+	reqData, _ := json.Marshal(req)
+	fmt.Println("req object:", string(reqData))
+
+	response := ExampleResponse{Message: fmt.Sprintf("Hello! Dear %s!", body.Name)}
+	return api_gateway.ApiResponse(200, response)
 }
 
 func main() {
